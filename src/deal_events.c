af30e14af45538b003e75ba749b6f949516b806d
madvid <mdavid@student.42.fr>
2021-03-13 16:00:17 +0100

    [m_FIX] retrait de printf.

--- src/deal_events.c
+++ src/deal_events.c
@@ -6,7 +6,7 @@
 /*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/19 19:09:28 by mdavid            #+#    #+#             */
-/*   Updated: 2021/03/12 11:10:59 by mdavid           ###   ########.fr       */
+/*   Updated: 2021/03/13 15:20:43 by mdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,6 @@ int		ft_key_press(int kcode, t_mlx *mlx)
 	if (kcode == UP || kcode == RIGHT || kcode == DOWN || kcode == LEFT
 		|| kcode == A || kcode == S || kcode == D || kcode == W)
 		ft_event_transl(kcode, mlx);
-	/*
-	if (kcode >= 83 && kcode <= 88)
-		ft_rotation_xyz(kcode, mlx, (*mlx)->events, (*mlx)->img);
-	if (kcode == 24 || kcode == 27)
-		ft_altitude(kcode, mlx, (*mlx)->events, (*mlx)->img);
-	if (kcode == 69 || kcode == 78)
-		ft_zoom(kcode, mlx, (*mlx)->events, (*mlx)->img);
-	if (kcode == 34 || kcode == 35)
-		ft_projections(kcode, mlx, (*mlx)->events, (*mlx)->img);
-	*/
 	return (0);
 }
 
@@ -76,7 +66,6 @@ void	ft_event_transl(int kcode, t_mlx *mlx)
 		mlx->img->origin.x += 10 * mlx->img->ratio;
 	if (kcode == W)
 		mlx->img->origin.y -= 10 * mlx->img->ratio;
-	printf("origin.x = %Lf -- origin.y = %Lf\n", mlx->img->origin.x, mlx->img->origin.y);
 	fractal_construct(mlx);
 	mlx_put_image_to_window(mlx->init, mlx->w_ptr, mlx->img->ptr, 0, W_LY / 10);
 }
@@ -123,22 +112,15 @@ int		ft_mouse_event(int mcode, int x, int y, t_mlx *mlx)
 	}
 	*/
 	if (mcode == SCROLL_UP) // scroll up
-	{
 		mlx->img->ratio = mlx->img->ratio * 0.9;
-		printf("valeur de ratio = %Lf\n", mlx->img->ratio);
-	}
 	if (mcode == SCROLL_DOWN) // scroll down
-	{
 		mlx->img->ratio = mlx->img->ratio * 1.1;
-		printf("valeur de ratio = %Lf\n", mlx->img->ratio);
-	}
 	(void)x;
 	(void)y;
 	fractal_construct(mlx);
 	mlx_put_image_to_window(mlx->init, mlx->w_ptr, mlx->img->ptr, 0, W_LY / 10);
 	return (0);
 }
-
 
 /*
 ** FONCTION : ft_mouse_move
@@ -154,10 +136,8 @@ int		ft_mouse_event(int mcode, int x, int y, t_mlx *mlx)
 
 int		ft_mouse_move(int mouse_x, int mouse_y, t_mlx *mlx)
 {
-	//t_gdad	gdad;
-	
-	mlx->img->cst.x = CX + 0.005 * (mouse_x - 0.5 * IMG_LX);
-	mlx->img->cst.y = CY + 0.005 * (mouse_y - 0.5 * IMG_LY);
+	mlx->img->cst.x = CST_X + 0.005 * (mouse_x - 0.5 * IMG_LX);
+	mlx->img->cst.y = CST_Y + 0.005 * (mouse_y - 0.5 * IMG_LY);
 	fractal_construct(mlx);
 	mlx_put_image_to_window(mlx->init, mlx->w_ptr, mlx->img->ptr, 0, W_LY / 10);
 	return (0);
