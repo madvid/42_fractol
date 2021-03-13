1c8dd3236935e0f8303035c30a660774589cd4a9
madvid <mdavid@student.42.fr>
2021-03-13 15:58:17 +0100

    [FEAT] fonction permettant de faire le produit entre 2 nombres complexes.

--- src/maths_tools1.c
+++ src/maths_tools1.c
@@ -6,7 +6,7 @@
 /*   By: mdavid <mdavid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/19 15:00:54 by mdavid            #+#    #+#             */
-/*   Updated: 2021/03/12 10:48:17 by mdavid           ###   ########.fr       */
+/*   Updated: 2021/03/13 15:51:07 by mdavid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,25 @@ t_fpt	associated_complex_coord(t_ipt p, t_img *img)
 
 	coord.x = img->ratio * (p.x - 0.5 * IMG_LX) - img->origin.x;
 	return (coord);
+}
+
+/*
+** FONCTION: complex_prod
+** PARAMETRES:
+**		z1 [t_fpt]: 1er nombre complexe de la multiplication
+**		z2 [t_fpt]: 2nd nombre complexe de la multiplication
+** DESCRIPTION:
+**		Produit entre 2 nombre complexes.
+**		La fonction n'est pas protégée contre les overflows pour les
+**		la multiplication entre float.
+**		Cela est conscient, la protection découle du semi-clipping du
+**		zoom (on ne peut pas dezoomer de manière infini).
+**	Remarque: Cela permet de ne pas ralentir.
+** RETOUR:
+**		result [t_fpt]: résultat du produit entre z1 et z2.
+*/
+
+t_fpt	complex_prod(t_fpt z1, t_fpt z2)
+{
+	return ((t_fpt){z1.x * z2.x - z1.y * z2.y, z1.x * z2.y + z1.y * z2.x, 0});
 }
